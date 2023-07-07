5ce64265658d773b0c1bcdb5cc59e570b18067cc
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2023-07-07 13:44:33 +0200

    prova

--- resources/views/admin/biograpies/index.blade.php
+++ resources/views/admin/biograpies/index.blade.php
@@ -1,4 +1,16 @@
 <x-app-layout>
-    
- 
+    <div class="container mx-auto py-4">
+        <div class="flex">
+            <div class="col">
+                <h1 class="text-4xl font-bold text-white">Biografia</h1>
+
+                <form action="">
+
+                    <textarea class="form-control"></textarea>
+
+                    <button>Aggiungi</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
